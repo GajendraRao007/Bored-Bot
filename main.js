2616f73fa857aaf3cbb2dcb92a0779f4e26d3077
Gajendra <raogajendra27@gmail.com>
2023-03-16 13:21:01 +0530

    Js added, Fetch data on click of button ,Completed project

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const clickBtn = document.getElementById("btn")
+
+clickBtn.addEventListener('click',function(){
+    
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {console.log(data)
+    
+        document.getElementById("activity").textContent = data.activity
+    
+    })
+    
+})
